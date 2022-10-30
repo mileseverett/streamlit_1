5b6d39167cf867a62e023e9619ba442d81a05e61
mileseverett <miles_everett@hotmail.co.uk>
2022-10-30 20:46:49 +0000

    update

--- myapp.py
+++ myapp.py
@@ -2,12 +2,16 @@ import streamlit as st
 
 import plotly.graph_objects as go
 
+from PIL import Image
+
+img = Image.open("images/de_ancient.png")
+
 # Create figure
 fig = go.Figure()
 
 fig.add_layout_image(
         dict(
-            source="images/de_ancient.png",
+            source=img,
             xref="x",
             yref="y",
             x=0,
@@ -20,13 +24,27 @@ fig.add_layout_image(
 )
 
 
-fig.add_trace(go.Scatter(x=[100,100,200,50], y=[100,200,100,50], fill="toself"))
+# fig.add_trace(go.Scatter(x=[100,100,200,50], y=[100,200,100,50], fill="toself"))
 
 fig.update_layout(template="plotly_white")
 
-fig.update_xaxes(showgrid=False, range=(0, 1024))
-fig.update_yaxes(showgrid=False, scaleanchor='x', range=(1024, 0))
+fig.update_xaxes(showgrid=False, range=(0, 1024), position=0)
+fig.update_yaxes(showgrid=False, scaleanchor='x', range=(0, 1024))
+
+fig.update_layout(
+    autosize=False,
+    width=2000,
+    height=2000,
+    margin=dict(
+        l=0,
+        r=0,
+        b=0,
+        t=0,
+        pad=0
+    ),
+    # paper_bgcolor="LightSteelBlue",
+)
 
 # fig.show()
 
-st.plotly_chart(fig)
+st.plotly_chart(fig, use_container_width=True)
